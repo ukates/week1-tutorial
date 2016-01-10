98955c01b0033b8289b93d8b27b18feaad0fa940
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-01-10 00:09:30 -0500

    test files

---
+++ hello.py
@@ -0,0 +1,6 @@
+# replace the word 'hector' with your own name
+
+def hello_world():
+	return 'hello world, my name is '
+
+print( hello_world() + 'hector' )
---
+++ test_hello.py
@@ -0,0 +1,4 @@
+import hello
+
+def test_answer():
+    assert hello.hello_world() == 'hello world, my name is '
