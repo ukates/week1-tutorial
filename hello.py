58d80de1f15b04495cd33c5647e8c875a807192a
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-01-11 14:23:06 -0500

    Step 4 of instructions

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is '
+	return 'hello world, my name is Derrick'
 
 print( hello_world() + 'hector' )
